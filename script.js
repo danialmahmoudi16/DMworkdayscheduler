c3a38ce3d183ff5e8d12c5591743d58efcfbb55d
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-02-26 18:37:59 -0500

    Added current date in header of the page

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var currentday = dayjs().format("MMM D, YYYY");
+
+$("#currentDay").text(currentday);
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
